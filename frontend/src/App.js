85c0d97ba26b7472b0a931e502d6f8eb0c12fc22
Christian-pprogrammer <mpanoc6@gmail.com>
2022-04-26 06:29:58 +0200

    shipping address route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import { LinkContainer } from 'react-router-bootstrap'
 import { ToastContainer } from 'react-toastify'
 
 import {Store} from './Store';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 
 function App() {
   const {state, dispatch: ctxDispatch} = useContext(Store);
@@ -70,6 +71,7 @@ function App() {
               <Route path='/product/:slug' element={<ProductScreen />} />
               <Route path='/cart' element={<CartScreen />} />
               <Route path='/signin' element={<SigninScreen />} />
+              <Route path='/shipping' element={<ShippingAddressScreen />} />
               <Route path='/' element={<HomeScreen />} />
             </Routes>
           </Container>
