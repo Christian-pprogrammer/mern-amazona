47f9dd940a8e7eef651988356a0574e1c2591437
Christian-pprogrammer <mpanoc6@gmail.com>
2022-12-09 21:27:05 +0200

    last commit

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import userRouter from './routes/userRoutes.js';
 import productRouter from './routes/productRoutes.js';
 import orderRouter from './routes/orderRoutes.js';
 
-dotenv.config();
+dotenv.config({path: './.env'});
 
 mongoose
   .connect(process.env.MONGODB_URI)
